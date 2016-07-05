bfd15df0ebca4cb8b2b061f65ff766d5ddc1dfe8
ajdepersio <ajdepersio@gmail.com>
2016-07-05 12:30:11 -0400

    Updates to Navbar positioning

--- js/main.js
+++ js/main.js
@@ -10,3 +10,7 @@ $(window).resize(onResize);
 $(function() {
   onResize();
 });
+
+var shiftWindow = function() { scrollBy(0, -50) };
+if (location.hash) shiftWindow();
+window.addEventListener("hashchange", shiftWindow);
