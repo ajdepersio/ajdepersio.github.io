f5cb4dc2bec089a7181667cc2faad9dd5b6572a4
Ajdepersio <andrew.depersio@inin.com>
2017-10-29 16:43:56 -0400

    Started work on populateJobExperience()

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
+var m_experience = [
+  {
+    Company = "RenPSG",
+    Logo = "assets/renpsg.png",
+    Title = "<p><strong>Senior Software Engineer -</strong> IT</p>",
+    StartDate = new Date(2017, 5, 19),
+    EndDate = new Date(),
+    Description = "Blah blah blah blah blah, \nsome stuff that I did \nA new line"
+  },
+  {
+    Company = "Genesys",
+    Logo = "assets/genesys-bubble.png",
+    Title = "<p><strong>Senior Software Engineer -</strong> IT</p>",
+    StartDate = new Date(2017, 5, 19),
+    EndDate = new Date(),
+    Description = "Blah blah blah blah blah, \nsome stuff that I did \nA new line"
+  },
+  {
+    Company = "Interactive Intelligence",
+    Logo = "assets/inin-cloud.png",
+    Title = "<p><strong>Senior Software Engineer -</strong> IT</p>",
+    StartDate = new Date(2017, 5, 19),
+    EndDate = new Date(),
+    Description = "Blah blah blah blah blah, \nsome stuff that I did \nA new line"
+  },
+  {
+    Company = "Oak Ridge National Laboratory",
+    Logo = "assets/ornl.png",
+    Title = "<p><strong>SULI Intern -</strong> Computational Sciences Research and Development</p>",
+    StartDate = new Date(2017, 5, 19),
+    EndDate = new Date(),
+    Description = "Blah blah blah blah blah, \nsome stuff that I did \nA new line"
+  }
+];
+
+function populateJobExperience() {
+  var container = $('#experience-row');
+  
+  var ele;
+  for (var i = 0; i < m_experience.Count; i++) {
+    ele = "<div";
+  }
+};
+
 var onResize = function() {
   // apply dynamic padding at the top of the body according to the fixed navbar height
   $("#document_container").css("padding-top", $(".navbar-fixed-top").height());
