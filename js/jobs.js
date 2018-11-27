8cd984099e9638064d342ccc6c16f95e40d1cd7e
depersioa <depersioa@reninc.com>
2018-11-27 00:00:35 -0500

    Mostly styling updates.  Removed Bootstrap.

--- js/jobs.js
+++ js/jobs.js
@@ -60,21 +60,28 @@ m_jobs.forEach(job => {
 
 $.fn.populateJobInfo = function(){
     var header = '<div class="tabs"><div class="row">';
-    var nav = '<div class="col-4"><nav class="tab-nav tabs-nav-block">';
+    var nav = '<div class="col-4"><nav class="tabs-nav tabs-nav-block">';
     var content = '<div class="col-8">';
 
     //Add content
     var navItem;
     var contentItem;
+    var contentId;
     for (let i = 0; i < m_jobs.length; i++) {
         var job = m_jobs[i];
+        contentId = 'job-' + i;
+        navItem = '<a href="#' + contentId + '" class="orange">' + job.Label + '</a>';
+        contentItem = '<div class="tabs-pane" id="' + contentId + '"><h4>' + job.Department + '</h4></div>';
         
-        
+        nav = nav + navItem;
+        content = content + contentItem;
     }
     
     var contentClose = '</div>';
     var navClose = '</nav></div>';
     var footer = '</div></div>';
-    
-    
+
+    var ele = header + nav + navClose + content + contentClose + footer;
+
+    $('#experience-container').append(ele);
 };
